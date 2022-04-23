cac95f5bf1ca6a57556d4ea4785b94feed1a612b
hihasan <hihasan07@gmail.com>
2022-04-24 01:23:55 +0600

    update git ignor

---
+++ app/src/main/java/com/hihasan/richtexteditor/SplashActivity.kt
@@ -0,0 +1,10 @@
+package com.hihasan.richtexteditor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/hihasan/richtexteditor/adapter/FontSettingAdapter.kt
@@ -0,0 +1,4 @@
+package com.hihasan.richtexteditor.adapter
+
+class FontSettingAdapter(s : String, baseViewHolder : BaseViewHolder) : BaseQuickAdapter<s, baseViewHolder> {
+}
