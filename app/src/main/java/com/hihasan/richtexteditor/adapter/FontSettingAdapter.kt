7989b78db8786a27407c64718dcb1a79bee4a210
hihasan <hihasan07@gmail.com>
2022-07-13 01:43:51 +0600

    Layout files

--- app/src/main/java/com/hihasan/richtexteditor/adapter/FontSettingAdapter.kt
+++ app/src/main/java/com/hihasan/richtexteditor/adapter/FontSettingAdapter.kt
@@ -1,4 +1,4 @@
 package com.hihasan.richtexteditor.adapter
 
-class FontSettingAdapter(s : String, baseViewHolder : BaseViewHolder) : BaseQuickAdapter<s, baseViewHolder> {
+class FontSettingAdapter{
 }
